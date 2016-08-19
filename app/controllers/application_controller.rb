01fe3b1f80a5bc3790b1c3d0377c7f1100b6de2b
sameesh-s <sameesh.fisat@gmail.com>
2016-08-19 15:36:09 +0530

    Login success

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  def hello
-    render html:"Sample_Application 3"
-  end
+  include SessionsHelper
 end
