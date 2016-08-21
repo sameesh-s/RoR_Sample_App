1f8a74670288d6d0a003ead78b3660868125e539
sameesh-s <sameesh.fisat@gmail.com>
2016-08-21 20:01:25 +0530

    Forgot use

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,13 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
-  validates :name,
-           presence: true,
-           length: { minimum:5, maximum: 50 }
-  validates :email,
-           presence: true,
-           length: { minimum:8, maximum: 255 },
-           format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
-           uniqueness: { case_sensitive: false }
+  validates :name,  presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
   has_secure_password
-  validates :password,
-           presence: true,
-           length: { minimum: 6 },
-           allow_nil: true
+  validates :password, presence: true, length: { minimum: 6 }
 
   # Returns the hash digest of the given string.
   def User.digest(string)
@@ -21,4 +16,24 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
 
+  # Returns a random token.
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Remembers a user in the database for use in persistent sessions.
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the given token matches the digest.
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Forgets a user.
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
